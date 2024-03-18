2cbb9f3c952773a3c2da6750cb6471107877761f
Georgi Videv <104200628+gvidev@users.noreply.github.com>
2024-03-18 16:32:15 +0200

    basic design idea

--- FoodNutrientsCalc/src/App.tsx
+++ FoodNutrientsCalc/src/App.tsx
@@ -1,35 +1,38 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Table from "./components/Table";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [tableColor, setTableColor] = useState("black");
 
   const handleButtonClick = () => {
     setCount(count + 1);
+    setTableColor("red");
   };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="ui grid">
+        <div className="four wide columns">
+          <div className="ui icon input left">
+            <input type="text" placeholder="Search..."></input>
+            <i className="circular search link icon"></i>
+          </div>
+        </div>
+
+        <div className="two wide columns">
+          <div className="ui icon add right">
+            <i className="circular add icon"></i>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
+
+      <Table color={"" + tableColor + ""}></Table>
+
       <div className="card">
         <button onClick={handleButtonClick}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
